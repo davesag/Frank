1b5cf701eda23ec117d9699361d9e150c5b154ee
Dave Sag <davesag@gmail.com>
2010-09-07 09:08:37 +1000

    added simple user login interaction

--- shelloworld.rb
+++ shelloworld.rb
@@ -3,11 +3,26 @@ require 'rubygems'
 require 'sinatra'
 require 'haml'
 
+aDefaultMessage = "Hello"
+
 get '/' do
-	haml :index, :locals => { :name => "World"}
+	haml :index, :locals => { :name => "World", :message => aDefaultMessage}
 end
 
 get '/hello/:name' do
   aName = params[:name]
-	haml :index, :locals => { :name => aName }
+	haml :index, :locals => { :name => aName, :message => aDefaultMessage }
+end
+
+get '/login' do
+	haml :login, :locals => { :message => 'Please log in' }
+end
+
+post '/' do
+  aName = params['username']
+  if aName == 'Dave'
+    haml :index, :locals => { :name => aName, :message => "Welcome" }
+  else
+    haml :login, :locals => { :message => "Unknown User" }
+  end
 end
