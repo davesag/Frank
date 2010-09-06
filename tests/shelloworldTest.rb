ec80a6423850f14143d5985dabfc3380063731c3
Dave Sag <davesag@gmail.com>
2010-09-06 15:53:26 +1000

    added simple unit test

---
+++ tests/shelloworldTest.rb
@@ -0,0 +1,25 @@
+# indexTest.rb
+require 'shelloworld'
+require 'test/unit'
+require 'rack/test'
+
+class ShelloworldTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+
+  def app
+    Sinatra::Application
+  end
+
+  def test_my_default
+    get '/'
+    assert last_response.ok?
+    assert last_response.body.include?('Hello World')
+  end
+
+  def test_with_params
+    get '/hello/Dave'
+    assert last_response.ok?
+    assert last_response.body.include?('Hello Dave')
+  end
+ 
+end
